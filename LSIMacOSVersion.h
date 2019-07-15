5fed5c0747cdb6b31f11a35e59e413f6417fea15
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-15 10:40:38 -0600

    Add LSI files

---
+++ LSIMacOSVersion.h
@@ -0,0 +1,17 @@
+//
+//  LSIMacOSVersion.h
+//  MacOSVersions
+//
+//  Created by Mitchell Budge on 7/15/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIMacOSVersion : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LSIMacOSVersion.m
@@ -0,0 +1,13 @@
+//
+//  LSIMacOSVersion.m
+//  MacOSVersions
+//
+//  Created by Mitchell Budge on 7/15/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import "LSIMacOSVersion.h"
+
+@implementation LSIMacOSVersion
+
+@end
